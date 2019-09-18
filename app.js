d7e44b7479b4a24e0dc2da1eecf544da5161b98a
Martin Olsson <malvack@gmail.com>
2019-09-18 11:02:10 +0200

    0.01

---
+++ app.js
@@ -0,0 +1,50 @@
+// Deklarerar variabler
+const canvas = document.querySelector('canvas');
+const gfx = canvas.getContext("2d");
+let colors = document.getElementsByClassName('color');
+let painting = false;
+
+// Ändrar storleken på canvas elementet till fönsterstorleken.
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight - 50; // Räknar bort Toolbar diven.
+
+// Loopa igenom colors array och lägg till en klick event på elementet.
+for (let i = 0; i < colors.length; i++) {
+    colors[i].addEventListener('click', valjFarg);
+}
+
+function valjFarg(e) {
+    // Vilken färg klickades på?
+    let color = e.target;
+    // Sätt penfärg till backgrundsfärgen på knappen.
+    gfx.strokeStyle = color.style.backgroundColor; 
+}
+
+function startPosition(e) {
+    painting = true;
+    paint(e); 
+}
+
+function slutPosition() {
+    painting = false;
+    // Skapa en ny path när man släpper musknappen.
+    gfx.beginPath(); 
+}
+
+function paint(e) {
+    if(!painting) return; // Hoppa ur funktionen om musknappen inte trycks ner.
+    gfx.lineWidth = 10; // Bredden på "pennan".
+    gfx.lineCap = "round"; // Rundare kanter på linjerna.
+    gfx.lineTo(e.clientX, e.clientY); // Drar en linje mellan mus x och mus y positionen.
+    gfx.stroke(); // Ritar där muspelaren befinner sig.
+    gfx.beginPath(); // Skapa en ny path när man släpper musknappen.
+    gfx.moveTo(e.clientX, e.clientY); // Flyttat linjen till mus positionen.
+}
+
+// Deklarera eventlisteners
+canvas.addEventListener('mousedown', startPosition);
+canvas.addEventListener('mouseup', slutPosition);
+canvas.addEventListener('mousemove', paint);
+
+
+
