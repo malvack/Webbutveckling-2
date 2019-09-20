6dfd5533e25c782fecb11e8bf1b6bf00e8682d08
Martin Olsson <malvack@gmail.com>
2019-09-20 12:01:17 +0200

    0.1

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Deklarerar variabler
 const canvas = document.querySelector('canvas');
 const gfx = canvas.getContext("2d");
+const hamburgaren = document.querySelector('#hamburgaren');
+const menyLinks = document.querySelector('#menyLinks');
+const links = document.querySelectorAll('#menyLinks li');
 let colors = document.getElementsByClassName('color');
 let painting = false;
 
@@ -14,10 +17,8 @@ for (let i = 0; i < colors.length; i++) {
 }
 
 function valjFarg(e) {
-    // Vilken färg klickades på?
-    let color = e.target;
-    // Sätt penfärg till backgrundsfärgen på knappen.
-    gfx.strokeStyle = color.style.backgroundColor; 
+    let color = e.target; // Vilken färg klickades på?
+    gfx.strokeStyle = color.style.backgroundColor; // Sätt penfärg till backgrundsfärgen på knappen.
 }
 
 function startPosition(e) {
@@ -26,13 +27,18 @@ function startPosition(e) {
 }
 
 function slutPosition() {
-    painting = false;
-    // Skapa en ny path när man släpper musknappen.
-    gfx.beginPath(); 
+    painting = false;    
+    gfx.beginPath(); // Skapa en ny path när man släpper musknappen.
 }
 
 function paint(e) {
     if(!painting) return; // Hoppa ur funktionen om musknappen inte trycks ner.
+    
+    /*  PreventDefault löste problemet med att rita utanför kanterna.
+        Man markerade text istället för att kunna klicka på knapparna.
+        Källa: https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault */
+    e.preventDefault();
+
     gfx.lineWidth = 10; // Bredden på "pennan".
     gfx.lineCap = "round"; // Rundare kanter på linjerna.
     gfx.lineTo(e.clientX, e.clientY); // Drar en linje mellan mus x och mus y positionen.
@@ -41,15 +47,12 @@ function paint(e) {
     gfx.moveTo(e.clientX, e.clientY); // Flyttat linjen till mus positionen.
 }
 
+function menyToggle() {
+    menyLinks.classList.toggle("open"); // Byt mellan #menyLinks och #menyLinks.open 
+}
+
 // Deklarera eventlisteners för Desktop mus
 canvas.addEventListener('mousedown', startPosition);
 canvas.addEventListener('mouseup', slutPosition);
 canvas.addEventListener('mousemove', paint);
-
-// Deklarera eventlisteners för Mobile touch
-canvas.addEventListener('touchstart', startPosition);
-canvas.addEventListener('touchend', slutPosition);
-canvas.addEventListener('touchmove', paint);
-
-
-
+hamburgaren.addEventListener('click', menyToggle);
